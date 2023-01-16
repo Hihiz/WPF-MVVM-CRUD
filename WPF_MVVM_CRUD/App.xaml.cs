f12825d601cdf792a2e31e4bcecd093523467dc8
Hihiz <public@mail.ru>
2023-01-16 23:35:39 +0300

    Revert "l"
    This reverts commit e589d1c257021e137e9f86b24554bb40beae1bc2.

--- WPF_MVVM_CRUD/App.xaml.cs
+++ WPF_MVVM_CRUD/App.xaml.cs
@@ -1,9 +1,17 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace WPF_MVVM_CRUD
 {
-    public partial class App
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
     {
-
     }
 }
